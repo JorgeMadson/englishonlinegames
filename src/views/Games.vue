ca467887d212eb7b6cae674c652135f4a9404d1d
Jorge Madson <jorgemadsonsv@gmail.com>
2022-03-03 22:21:48 -0300

    Creating components on the fly

--- src/views/Games.vue
+++ src/views/Games.vue
@@ -4,27 +4,46 @@
   <div class="container">
     <fieldset>
       <legend>What is the time of question?</legend>
-      <input type="radio" name="question_type" value="qa" v-model="question" />Question & Answer<br>
-      <input type="radio" name="question_type" value="puzzle" v-model="question" />Puzzle<br>
-      <input type="radio" name="question_type" value="multiplechoice" v-model="question" />Multiple Choice<br>
+      <input
+        type="radio"
+        name="question_type"
+        value="qa"
+        v-model="question"
+      />Question & Answer<br />
+      <input
+        type="radio"
+        name="question_type"
+        value="puzzle"
+        v-model="question"
+      />Puzzle<br />
+      <input
+        type="radio"
+        name="question_type"
+        value="multiplechoice"
+        v-model="question"
+      />Multiple Choice<br />
       <button @click="addToGame(question)">Add</button>
+      <p>Number of questions: {{game.length}}</p>
     </fieldset>
-  <div>
-    <p>Here is to show how the game will look like:</p>
-    <form action="">
-      <fieldset>
-      <label for="question">Question</label> <br>
-      <input type="text" name="answer" id="" placeholder="Answer">
+    <div>
+      <p>Here is to show how the game will look like:</p>
+
+      <fieldset v-for="question in game">
+        <div v-if="question === 'qa'">
+          <label for="question">Question</label> <br />
+          <input type="text" name="answer" id="" placeholder="Corret answer" />
+          <input type="text" name="hint" id="" placeholder="Hint" />
+        </div>
+        <div v-if="question === 'multiplechoice'">
+          <label for="question">Choose</label>
+          <input type="radio" name="question" id=""> 1
+          <input type="radio" name="question" id=""> 2
+          <input type="radio" name="question" id=""> 3
+        </div>
+        <div v-if="question === 'puzzle'">
+          <p>Puzzle</p>
+        </div>
       </fieldset>
-      <fieldset>
-      <label for="question">Question</label> <br>
-      <input type="text" name="answer" id="" placeholder="Answer">
-      </fieldset>
-    </form>
-  </div>
-  <div>
-      <code>{{question}}</code>
-      <code>{{game}}</code>
     </div>
   </div>
 
@@ -35,16 +54,16 @@
 export default {
   name: "Games",
   components: {},
-   data() {
+  data() {
     return {
       game: [],
-      question: null
-    }
+      question: null,
+    };
   },
   methods: {
     addToGame(question) {
       this.game.push(question);
-    }
+    },
   },
 };
 </script>
@@ -52,6 +71,5 @@ export default {
 <style scoped>
 .container {
   display: flex;
-  
 }
 </style>
